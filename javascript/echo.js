3e4431a9a9defff9efdc7342a99e7becc3fd7c18
hsbatta <43866874+hsbatta@users.noreply.github.com>
2019-09-25 00:20:07 -0400

    Add files via upload

---
+++ javascript/echo.js
@@ -0,0 +1,6 @@
+var Continue = true;
+while(prompt != "Exit"){
+const readline = require('readline-sync');
+var prompt = readline.question('Enter a prompt: ');
+console.log("You Entered: " + prompt)
+}
