ecb02bda5858ad94772cfbe9ab6e4851bc9de99a
anishtoomu <anishtoomu@gmail.com>
2019-10-02 13:07:39 -0400

    added myself as a user

---
+++ kotlin/Calculator/Calculator.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    var num1 = 4
+    var num2 = 4
+    var answer1
+
+    println(answer1)
+
+
+
+}
+
+fun sum() {
+    num1 + num2 = answer1
+}
+
+fun difference() {
+    num1 - num2 = answer2
+}
+
+fun product() {
+    num1*num2 = answer3
+}
+
+fun quotient() {
+    num1/num2 = answer4
+}
